39b456ce900223a7f93797d0f13eff4702cffa4e
zhaoqi <154809748@qq.com>
2020-03-10 23:51:04 +0800

    test: 更新测试文件

--- packages/zeroer-core/test/api.js
+++ packages/zeroer-core/test/api.js
@@ -1,46 +1,46 @@
 var z = zeroerCore // eslint-disable-line
 console.log(z)
 
-// Array
+// array
 console.log(
-  'Array.flat ',
-  z.Array.flat([1, [2, [3]]], 1),
-  z.Array.flat([1, [2, [3]]])
+  'array.flat ',
+  z.array.flat([1, [2, [3]]], 1),
+  z.array.flat([1, [2, [3]]])
 )
-console.log('Array.remove ', z.Array.remove([1, 2, 3], 2))
-console.log('Array.subtract ', z.Array.subtract([1, 2, 3], [2, 3]))
+console.log('array.remove ', z.array.remove([1, 2, 3], 2))
+console.log('array.subtract ', z.array.subtract([1, 2, 3], [2, 3]))
 
-// Browser
-console.log('Browser.userAgent', z.Browser.userAgent)
+// browser
+console.log('browser.userAgent', z.browser.getUserAgent())
 
-// Cookie
-z.Cookie.set('a', 'aVal', '2019/09/04 16:50') // 存储到'2019/09/04 16:50'
-z.Cookie.set('b', 'bVal', 1, '/') // 存储1天
-z.Cookie.set('c', 'cVal') // 当前会话期间
-// z.Cookie.del('b', '/')
+// cookie
+z.cookie.set('a', 'aVal', '2019/09/04 16:50') // 存储到'2019/09/04 16:50'
+z.cookie.set('b', 'bVal', 1, '/') // 存储1天
+z.cookie.set('c', 'cVal') // 当前会话期间
+// z.cookie.del('b', '/')
 console.log(
-  'Cookie.get ',
-  z.Cookie.get('a'),
-  z.Cookie.get('b'),
-  z.Cookie.get('c')
+  'cookie.get ',
+  z.cookie.get('a'),
+  z.cookie.get('b'),
+  z.cookie.get('c')
 )
 
-// Date
-console.log('Date.format ',
-  z.Date.format(Date.now(), 'YYYY-MM-DD HH:mm:ss'),
-  z.Date.format('2019/09/04 13:00', 'YYYY-MM-DD HH:mm:ss'),
-  z.Date.format('2019-09-04 13:00', 'YYYY-MM-DD HH:mm:ss'),
-  z.Date.format(new Date()),
-  z.Date.format(new Date().getTime(), 'YYYY-MM-DD a hh:mm:ss')
+// date
+console.log('date.format ',
+  z.date.format(Date.now(), 'YYYY-MM-DD HH:mm:ss'),
+  z.date.format('2019/09/04 13:00', 'YYYY-MM-DD HH:mm:ss'),
+  z.date.format('2019-09-04 13:00', 'YYYY-MM-DD HH:mm:ss'),
+  z.date.format(new Date()),
+  z.date.format(new Date().getTime(), 'YYYY-MM-DD a hh:mm:ss')
 )
-console.log('Date.diff ',
-  z.Date.diff(new Date('2018/01/01').getTime(), new Date('2018/01/02 13:00:00').getTime(), 'H'),
-  z.Date.diff('2018-01-01', '2018/01/02 13:00:00', 'H'),
-  z.Date.diff(new Date('2018/01/01'), new Date('2018/01/02 13:00:00'), 'H'),
-  z.Date.diff('2018/01/01', new Date('2018/01/02 13:00:00'), 'H')
+console.log('date.diff ',
+  z.date.diff(new Date('2018/01/01').getTime(), new Date('2018/01/02 13:00:00').getTime(), 'H'),
+  z.date.diff('2018-01-01', '2018/01/02 13:00:00', 'H'),
+  z.date.diff(new Date('2018/01/01'), new Date('2018/01/02 13:00:00'), 'H'),
+  z.date.diff('2018/01/01', new Date('2018/01/02 13:00:00'), 'H')
 )
 
-// Env
+// env
 
 // Event
 var event = new z.Event()
@@ -56,127 +56,125 @@ event.emit('customEventName2', 2)
 event.emit('customEventName3', 3)
 event.emit('customEventName3', 3)
 
-// LocalStorage
-z.LocalStorage.setItem('a', { a: 1 }) // 随浏览器进程
-z.LocalStorage.setItem('b', { b: 1 }, 1) // 存1天
-z.LocalStorage.setItem('c', { c: 1 }, '2020/03/07')
-// z.LocalStorage.removeItem('c')
-// z.LocalStorage.clear()
+// localStorage
+z.localStorage.setItem('a', { a: 1 }) // 随浏览器进程
+z.localStorage.setItem('b', { b: 1 }, 1) // 存1天
+z.localStorage.setItem('c', { c: 1 }, '2020/03/07')
+// z.localStorage.removeItem('c')
+// z.localStorage.clear()
 console.log(
-  'LocalStorage.getItem ',
-  z.LocalStorage.getItem('a'),
-  z.LocalStorage.getItem('b'),
-  z.LocalStorage.getItem('c')
+  'localStorage.getItem ',
+  z.localStorage.getItem('a'),
+  z.localStorage.getItem('b'),
+  z.localStorage.getItem('c')
 )
 
-// Number
+// number
 console.log(
-  'Number.complement ',
-  z.Number.complement(12, 2),
-  z.Number.complement(-1, 2),
-  z.Number.complement(-1, -2)
+  'number.complement ',
+  z.number.complement(12, 2),
+  z.number.complement(-1, 2),
+  z.number.complement(-1, -2)
 )
-console.log('Number.random ', z.Number.random(1, 10))
+console.log('number.random ', z.number.random(1, 10))
 
-// Object
-console.log('Object.isNull ', z.Object.isNull(null), z.Object.isNull(undefined))
-console.log('Object.isUndefined ', z.Object.isUndefined(undefined), z.Object.isUndefined(null))
-console.log('Object.isNumber ', z.Object.isNumber(1), z.Object.isNumber('1'))
-console.log('Object.isString ', z.Object.isString(''), z.Object.isString(1))
-console.log('Object.isBoolean ', z.Object.isBoolean(true), z.Object.isBoolean(''))
-console.log('Object.isArray ', z.Object.isArray([]), z.Object.isArray({}))
-console.log('Object.isObject ', z.Object.isObject({}), z.Object.isObject(function () {}))
-console.log('Object.isFunction ', z.Object.isFunction(function () {}), z.Object.isFunction({}))
-console.log('Object.isDomElement ', z.Object.isDomElement(document.getElementById('dom')), z.Object.isDomElement(1))
-console.log('Object.isRegExp ', z.Object.isRegExp(/asd/), z.Object.isRegExp({}))
-console.log('Object.isDate ', z.Object.isDate(new Date()), z.Object.isDate({}))
-console.log('Object.isBuffer ', true, z.Object.isBuffer({}))
+// object
+console.log('object.isNull ', z.object.isNull(null), z.object.isNull(undefined))
+console.log('object.isUndefined ', z.object.isUndefined(undefined), z.object.isUndefined(null))
+console.log('object.isNumber ', z.object.isNumber(1), z.object.isNumber('1'))
+console.log('object.isString ', z.object.isString(''), z.object.isString(1))
+console.log('object.isBoolean ', z.object.isBoolean(true), z.object.isBoolean(''))
+console.log('object.isArray ', z.object.isArray([]), z.object.isArray({}))
+console.log('object.isObject ', z.object.isObject({}), z.object.isObject(function () {}))
+console.log('object.isFunction ', z.object.isFunction(function () {}), z.object.isFunction({}))
+console.log('object.isDomElement ', z.object.isDomElement(document.getElementById('dom')), z.object.isDomElement(1))
+console.log('object.isRegExp ', z.object.isRegExp(/asd/), z.object.isRegExp({}))
+console.log('object.isDate ', z.object.isDate(new Date()), z.object.isDate({}))
+console.log('object.isBuffer ', true, z.object.isBuffer({}))
 var aObj1 = { obj: { a: 1 }, arr: [1, 2], v: 1 }
 var bObj1 = { obj: { b: 2 }, arr: [3, 4, 5], v: 2 }
-var cObj1 = z.Object.extend(aObj1, bObj1)
+var cObj1 = z.object.extend(aObj1, bObj1)
 var aObj2 = { obj: { a: 1 }, arr: [1, 2], v: 1 }
 var bObj2 = { obj: { b: 2 }, arr: [3, 4, 5], v: 2 }
-var cObj2 = z.Object.extend(true, aObj2, bObj2)
+var cObj2 = z.object.extend(true, aObj2, bObj2)
 console.log(
-  'Object.extend ',
+  'object.extend ',
   cObj1,
   cObj2
 )
 
-// Qs
+// qs
 console.log(
-  'Qs.stringify ',
-  z.Qs.stringify({ a: 1, b: { c: 'c', d: { e: 'e', r: { a: 1 } } }, c: 3 })
+  'qs.stringify ',
+  z.qs.stringify({ a: 1, b: { c: 'c', d: { e: 'e', r: { a: 1 } } }, c: 3 })
 )
 
 console.log(
-  'Qs.parse ',
-  z.Qs.parse('a=1&b%5Bc%5D=c&b%5Bd%5D%5Be%5D=e&b%5Bd%5D%5Br%5D%5Ba%5D=1&c=3')
+  'qs.parse ',
+  z.qs.parse('a=1&b%5Bc%5D=c&b%5Bd%5D%5Be%5D=e&b%5Bd%5D%5Br%5D%5Ba%5D=1&c=3')
 )
 
-// SessionStorage
-z.SessionStorage.setItem('a', { a: 1 })
-z.SessionStorage.setItem('b', '1')
-z.SessionStorage.setItem('c', 1)
-// z.SessionStorage.clear()
-z.SessionStorage.removeItem('b')
+// sessionStorage
+z.sessionStorage.setItem('a', { a: 1 })
+z.sessionStorage.setItem('b', '1')
+z.sessionStorage.setItem('c', 1)
+// z.sessionStorage.clear()
+z.sessionStorage.removeItem('b')
 console.log(
-  'SessionStorage.getItem ',
-  z.SessionStorage.getItem('a'),
-  z.SessionStorage.getItem('b'),
-  z.SessionStorage.getItem('c'),
+  'sessionStorage.getItem ',
+  z.sessionStorage.getItem('a'),
+  z.sessionStorage.getItem('b'),
+  z.sessionStorage.getItem('c'),
 )
 
-// String
+// string
 console.log(
-  'String.getQs ',
-  z.String.getQs(),
-  z.String.getQs('a'),
-  z.String.getQs({ url: 'http://localhost:4321/test/api.html?a=1&b=2#hash' }),
-  z.String.getQs({ url: 'http://localhost:4321/test/api.html?a=1&b=2#hash', data: 'a' })
+  'string.getQs ',
+  z.string.getQs(),
+  z.string.getQs('a'),
+  z.string.getQs({ url: 'http://localhost:4321/test/api.html?a=1&b=2#hash' }),
+  z.string.getQs({ url: 'http://localhost:4321/test/api.html?a=1&b=2#hash', data: 'a' })
 )
 
 console.log(
-  'String.setQs ',
-  z.String.setQs(),
-  z.String.setQs({ b: 1, s: 5 }),
-  z.String.setQs({ url: 'http://localhost:4321/test/api.html?a#hash', data: { s: 5 } }),
-  z.String.setQs({ url: 'http://localhost:4321/test/api.html?#hash', data: { s: 5 } }),
+  'string.setQs ',
+  z.string.setQs(),
+  z.string.setQs({ b: 1, s: 5 }),
+  z.string.setQs({ url: 'http://localhost:4321/test/api.html?a#hash', data: { s: 5 } }),
+  z.string.setQs({ url: 'http://localhost:4321/test/api.html?#hash', data: { s: 5 } }),
 )
 
-// Unicode
-console.log('Unicode.encode ', z.Unicode.encode('今天'))
-console.log('Unicode.decode ', z.Unicode.decode('\u4eca\u5929'))
+// unicode
+console.log('unicode.encode ', z.unicode.encode('今天'))
+console.log('unicode.decode ', z.unicode.decode('\u4eca\u5929'))
 
-// User
-
-// Util
-window.addEventListener('scroll', z.Util.throttle(function() {
-  console.log('Util.throttle ', this, 'agrs', arguments)
+// util
+window.addEventListener('scroll', z.util.throttle(function() {
+  console.log('util.throttle ', this, 'agrs', arguments)
 }, 400))
 
 var inputDom = document.getElementById('debounce')
-inputDom.addEventListener('input', z.Util.debounce(function () {
-  console.log('Util.debounce ', this, 'agrs', arguments)
+inputDom.addEventListener('input', z.util.debounce(function () {
+  console.log('util.debounce ', this, 'agrs', arguments)
 }, 400))
 
 var add = function (a, b, c) { return a + b + c }
-var curryAdd = z.Util.curry(add)
+var curryAdd = z.util.curry(add)
 console.log(
-  'Util.curry',
+  'util.curry',
   curryAdd(1)(2)(3),
   curryAdd(1, 2)(3),
   curryAdd(1)(2, 3)
 )
 
-var c = z.Util.compose(Math.abs, Math.pow)
+var c = z.util.compose(Math.abs, Math.pow)
 console.log(
-  'Util.compose',
+  'util.compose',
   c(-2, 3)
 )
 
-var p = z.Util.pipe(Math.pow, Math.abs)
+var p = z.util.pipe(Math.pow, Math.abs)
 console.log(
-  'Util.pipe',
+  'util.pipe',
   p(-2, 3)
 )
