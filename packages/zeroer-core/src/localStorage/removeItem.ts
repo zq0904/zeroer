b886503f64355452b0b5a0a6f1cd0749d149a78f
zhaoqi15 <zhaoqi15@58.com>
2020-07-14 10:46:12 +0800

    fix: 严谨的判断

--- packages/zeroer-core/src/localStorage/removeItem.ts
+++ packages/zeroer-core/src/localStorage/removeItem.ts
@@ -1,4 +1,5 @@
 import { PREFIX } from './lib'
+import getItem from './getItem'
 
 /**
  * 删除localStorage（只会删除由“该套方法”所存储的值 不会删除“其他”localStorage 基于前缀）
@@ -6,6 +7,7 @@ import { PREFIX } from './lib'
  * removeItem('a')
  */
 const removeItem = (name: string) => {
+  if (getItem(name) === null) return
   localStorage.removeItem(PREFIX + name)
 }
 
