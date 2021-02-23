378980d2ecf11969b24c3f30713da33188139972
zhaoqi15 <zhaoqi15@58.com>
2021-02-23 10:23:50 +0800

    fix: exec 修复

--- packages/zeroer-tool/src/utils/execA.ts
+++ packages/zeroer-tool/src/utils/execA.ts
@@ -6,12 +6,13 @@ import { log, logObj } from './log'
 const execA = (command: string, options: execa.Options = {}) => {
   const cp = execa.command(command, {
     preferLocal: true, // 在查找要执行的二进制文件时，首选本地安装的二进制文件 node_modules/.bin/ 下指令
+    shell: true, // 如果命令使用特定于shell的特性 必须开启 如 &&
     // localDir: process.cwd(), // 在中查找本地安装的二进制文件的首选路径（与结合使用preferLocal） 默认process.cwd()
     // cwd: process.cwd(), // 子进程的当前工作目录 默认 process.cwd()
     // stdin: 'inherit', // 默认pipe 以流的形式输入 一些如inquirer与用户交互的库希望从控制台直接输入
     // stdout: 'inherit', // 默认pipe 以流的形式输出 不直接在控制台输出 inherit 输出直接打印在控制台
     // stderr: 'inherit',
-    stdio: ['inherit', 'inherit', 'inherit', 'ipc'],
+    stdio: 'inherit',
     ...options
   })
   cp.on('error', (err) => {
